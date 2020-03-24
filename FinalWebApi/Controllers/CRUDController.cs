257c1aa8cadda555dd21b395d298f73d523d1ea3
unknown <mahendramanem17@gmail.com>
2020-03-24 12:38:14 +0530

    first commit

---
+++ FinalWebApi/Controllers/CRUDController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+using System.Net.Http;
+namespace FinalWebApi.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        public ActionResult Index()
+        {
+            IEnumerable<Emp> e = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:64250/api/Employee");
+            var consumeApi = hc.GetAsync("Employee");
+            consumeApi.Wait();
+            var readData = consumeApi.Result;
+            if(readData.IsSuccessStatusCode)
+            {
+                var displayData = readData.Content.ReadAsAsync<IList<Emp>>();
+                displayData.Wait();
+                e = displayData.Result;
+            }
+            return View(e);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Emp e)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:64250/api/Employee");
+            var insertRecord = hc.PostAsJsonAsync<Emp>("Employee", e);
+            insertRecord.Wait();
+            var saveData = insertRecord.Result;
+            if (saveData.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+                return View("Create");
+        }
+    }
+}
---
+++ FinalWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccessLayer;
+namespace FinalWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Emp> Get()
+        {
+            using (EmployeeDataEntities ed = new EmployeeDataEntities())
+            {
+                return ed.Emps.ToList();
+            }
+        }
+        public Emp Get(int id)
+        {
+            using (EmployeeDataEntities ed = new EmployeeDataEntities())
+            {
+                return ed.Emps.FirstOrDefault(e=>e.EmployeeId==id);
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Emp e)
+        {
+            try
+            {
+                using (EmployeeDataEntities ed = new EmployeeDataEntities())
+                {
+                    ed.Emps.Add(e);
+                    ed.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.Created, e);
+                    msg.Headers.Location = new Uri(Request.RequestUri + e.EmployeeId.ToString());
+                    return msg;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        public HttpResponseMessage Put(int id,[FromBody] Emp emp)
+        {
+            using (EmployeeDataEntities ed = new EmployeeDataEntities())
+            {
+                var entity = ed.Emps.FirstOrDefault(e => e.EmployeeId == id);
+                if (entity == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee Id =" + id.ToString());
+                }
+                else
+                {
+                    entity.Name = emp.Name;
+                    entity.Age = emp.Age;
+                    entity.Salary = emp.Salary;
+                    entity.Designation = emp.Designation;
+                    ed.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+
+                using (EmployeeDataEntities ed = new EmployeeDataEntities())
+                {
+                    var entity = ed.Emps.FirstOrDefault(e => e.EmployeeId == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee Id =" + id.ToString());
+                    }
+                    else
+                    {
+                        ed.Emps.Remove(ed.Emps.FirstOrDefault(e => e.EmployeeId == id));
+                        ed.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+        }
+    }
+}
